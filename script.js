a2ea3f9021bd5c8bdf023f156da0f6462d6021a8
Ibitayo Mercy <merxie1125@yahoo.com>
2024-07-02 21:01:27 +0100

    added current day

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Display current time in UTC
     const currentUTCTime = document.querySelector('[data-testid="currentTimeUTC"]');
+    const currentDay = document.querySelector('[data-testid="currentDay"]');
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     const updateTime = () => {
       const now = new Date();
       currentUTCTime.textContent = `${now.toUTCString()}`;
     };
     updateTime();
     setInterval(updateTime, 1000);
+
+    const today = new Date().getDay();
+    currentDay.textContent = `Current Day: ${daysOfWeek[today]}`;
   });
 
-      // Display current day of the week
-    // const currentDay = document.querySelector('[data-testid="currentDay"]');
-    // const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    // const today = new Date().getDay();
-    // currentDay.textContent = `Current Day: ${daysOfWeek[today]}`;
+   
+
+    
   
